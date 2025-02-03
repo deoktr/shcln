99de70822d55f058937a356911b397ee065e476d
deoktr <35725720+deoktr@users.noreply.github.com>
2025-02-03 09:44:02 +0100

    update: match regex

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ static MATCH_LIST: LazyLock<[Regex; 53]> = LazyLock::new(|| {
         Regex::new(r" --?[0-9a-zA-Z_-]*key(?: +|=)([^ ]+)").unwrap(),
         Regex::new(r" --?[0-9a-zA-Z_-]*jwt[0-9a-zA-Z_-]* *=?([^ ]+)").unwrap(),
         Regex::new(r" --?[0-9a-zA-Z_-]*secret[0-9a-zA-Z_-]* *=?([^ ]+)").unwrap(),
-        Regex::new(r"echo +(.*) *>.*(?:secret|pass|token|jwt)").unwrap(), // secret file creation
+        Regex::new(r"echo +(.*) *>.*(?:secret|pass|key|token|jwt)").unwrap(), // secret file creation
         // TODO: remove quotes from match group, note that rust regex doesn't
         // support look-around, and we need a single group, also cannot name
         // caputre group
@@ -43,32 +43,32 @@ static MATCH_LIST: LazyLock<[Regex; 53]> = LazyLock::new(|| {
         Regex::new(r#"echo (?:-e)?*["']?(.*)["']? *\| *sudo.*-S"#).unwrap(),
         Regex::new(r#"echo (?:-e)?*["']?(.*)["']? *\| *sudo.* passwd .*"#).unwrap(),
         Regex::new(r#"sudo.*-S.*<<< *(.*)"#).unwrap(),
-        Regex::new(r#"AWS_ACCESS_KEY_ID=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"AWS_SECRET_ACCESS_KEY=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r"(?i)(aws_access_key_id|aws_secret_access_key)=([0-9a-zA-Z/+]{20,40})").unwrap(),
+        Regex::new(r#"AWS_ACCESS_KEY_ID=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r#"AWS_SECRET_ACCESS_KEY=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r"(?i)(?:aws_access_key_id|aws_secret_access_key)=([0-9a-zA-Z/+]{20,40})").unwrap(),
         Regex::new(r#"(?i:authorization):(?:.*)(?i:Basic).(.*)("|'|\x60|\$\()"#).unwrap(),
         Regex::new(r"curl.*(?:-u|--user)(?:[ =])([^ ]*)").unwrap(),
-        Regex::new(r#"GITHUB_TOKEN=["']?([0-9a-zA-Z*_/+]{0,100})["']?"#).unwrap(),
+        Regex::new(r#"GITHUB_TOKEN=["']?([0-9a-zA-Z*_/+]{1,100})["']?"#).unwrap(),
         Regex::new(r"ghp_[0-9a-zA-Z]{36}").unwrap(),
         Regex::new(r"gho_[0-9a-zA-Z]{36}").unwrap(),
         Regex::new(r"(ghu|ghs)_[0-9a-zA-Z]{36}").unwrap(),
         Regex::new(r"ghr_[0-9a-zA-Z]{36}").unwrap(),
-        Regex::new(r#"GITLAB_TOKEN=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
+        Regex::new(r#"GITLAB_TOKEN=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
         Regex::new(r"glpat-[0-9a-zA-Z-_]{20}").unwrap(),
         Regex::new(r#"(?i)(?:heroku)(?:[0-9a-z\-_\t .]{0,20})(?:[\s|']|[\s|"]){0,3}(?:=|>|:=|\|\|:|<=|=>|:)(?:'|"|\s|=|\x60){0,5}([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})(?:['|"|\n|\r|\s|\x60]|$)"#).unwrap(),
-        Regex::new(r#"HEROKU_API_KEY=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"VAULT_TOKEN=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"CONSUL_HTTP_TOKEN=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"VERCEL_TOKEN=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"CLOUDFLARE_API_KEY=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"NEWRELIC_API_KEY=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
+        Regex::new(r#"HEROKU_API_KEY=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r#"VAULT_TOKEN=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r#"CONSUL_HTTP_TOKEN=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r#"VERCEL_TOKEN=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r#"CLOUDFLARE_API_KEY=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r#"NEWRELIC_API_KEY=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
         Regex::new(r#""key-[0-9a-zA-Z]{32}""#).unwrap(), // MailGun API Key
         Regex::new(r"(?i)[0-9a-f]{32}-us[0-9]{1,2}").unwrap(),
-        Regex::new(r"SG.[0-9A-Za-z\-_]{15,30}\.[0-9A-Za-z\-_]{15,30}").unwrap(),
-        Regex::new(r"SG\.[0-9A-Za-z\-_]{22}\.[0-9A-Za-z\-_]{43}").unwrap(),
+        Regex::new(r"(SG.[0-9A-Za-z\-_]{15,30}\.[0-9A-Za-z\-_]{15,30})").unwrap(),
+        Regex::new(r"(SG\.[0-9A-Za-z\-_]{22}\.[0-9A-Za-z\-_]{43})").unwrap(),
         Regex::new(r"(xox[pboa]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-zA-Z0-9]{30,32})").unwrap(),
         Regex::new(r"https://hooks.slack.com/services/([A-Za-z0-9+/]{44,46})").unwrap(),
-        Regex::new(r"SK[0-9a-fA-F]{32}").unwrap(),
+        Regex::new(r"(SK[0-9a-fA-F]{32})").unwrap(),
         Regex::new(r"(([A-Za-z]*:(?://)?)([-;:&=\+\$,\w]+)@[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:/[\+~%/.\w\-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?").unwrap(),
         Regex::new(r#"(?i)\b(AIza[0-9A-Za-z\\-_]{35})(?:['|"|\n|\r|\s|\x60]|$)"#).unwrap(),
         Regex::new(r"https://outlook.office.com/webhook/([0-9a-f-]{36})/@").unwrap(),
@@ -77,11 +77,11 @@ static MATCH_LIST: LazyLock<[Regex; 53]> = LazyLock::new(|| {
         Regex::new(r#"(?i)twitter(.{0,20})?[''"]([0-9a-z]{18,25})[''"]"#).unwrap(),
         Regex::new(r"wget(?:.*)--(?:ftp-|http-)?(?:password|user)[ =]([^ ]*)").unwrap(),
         Regex::new(r#"(?i)linkedin(.{0,20})?(?-i)[''"]([0-9a-zA-Z]{12,16})[''"]"#).unwrap(),
-        Regex::new(r"EAACEdEose0cBA[0-9A-Za-z]+").unwrap(),
+        Regex::new(r"(EAACEdEose0cBA[0-9A-Za-z]+)").unwrap(),
         Regex::new(r#"(?i)(?:datadog)(?:[0-9a-z\-_\t .]{0,20})(?:\[s|']|[\s|"]){0,3}(?:=|>|:=|\|\|:|<=|=>|:)(?:'|"|\s|=|\x60){0,5}([a-z0-9]{40})(?:['|"|\n|\r|\s|\x60|;]|$)"#).unwrap(),
         Regex::new(r#"README_API_KEY=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r#"CARGO_REGISTRY_TOKEN=["']?([0-9a-zA-Z*/+]{0,100})["']?"#).unwrap(),
-        Regex::new(r"diskutil.*passphrase ([[:alpha:][:punct:]]{0,200}).*").unwrap(),
+        Regex::new(r#"CARGO_REGISTRY_TOKEN=["']?([0-9a-zA-Z*/+]{1,100})["']?"#).unwrap(),
+        Regex::new(r"diskutil.*passphrase ([[:alpha:][:punct:]]{1,200}).*").unwrap(),
     ]
 });
 
@@ -89,8 +89,11 @@ static KEEP_TMP: bool = true;
 
 fn main() {
     let home_path = env::var("HOME").unwrap();
+    clean_home(&home_path);
+}
 
-    let base_path = Path::new(&home_path).join(BASH_HISTORY_FILE_PATH);
+fn clean_home(home_path: &str) {
+    let base_path = Path::new(home_path).join(BASH_HISTORY_FILE_PATH);
     let history_path = base_path.display().to_string();
     let tmp_path = base_path.with_extension("bak").display().to_string();
 
